5892531a9b4424c1d08566d294e8d2772e44c5be
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2023-09-28 16:03:55 +0700

    update: routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { authGuard } from './core/guards/auth.guard';
 const routes: Routes = [
   {
     path: '',
-    redirectTo: '/administration',
+    redirectTo: '/administration/dashboard',
     pathMatch: 'full',
   },
   {
--- src/app/management/dashboard/dashboard-routing.module.ts
+++ src/app/management/dashboard/dashboard-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
 
 const routes: Routes = [
+
   {
     path:'',
     component: DashboardComponent,
