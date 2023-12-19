5ed55664213c0dd5ff5da7a37be446fc7ba2c47e
Trịnh Hoàng Phúc <hoangphuc01022002@gmail.com>
2023-12-19 11:31:28 +0700

    update: enpoint to deploy

--- src/app/core/constants/url.constant.ts
+++ src/app/core/constants/url.constant.ts
@@ -7,7 +7,7 @@ export const URLConstant = {
       DRIVER: '/driver'
     },
     DRIVER: {
-      ENDPOINT: environment.serverTestUrl,
+      ENDPOINT: environment.serverUrl,
       AUTH: {
         LOGIN: '/auth/driver/signin',
         LOGOUT: '/auth/driver/logout',
@@ -15,7 +15,7 @@ export const URLConstant = {
       },
     },
     ADMIN: {
-      ENDPOINT: environment.serverTestUrl,
+      ENDPOINT: environment.serverUrl,
       AUTH: {
         LOGIN: '/auth/admin/signin',
         LOGOUT: '/auth/admin/logout',
